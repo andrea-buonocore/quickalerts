ad9a87cc638dc5dbb6ed07be72905100e4ff289b
andrea-buonocore <andreabuonocore00@gmail.com>
2023-06-29 01:47:21 +0200

    modificato button in dark mode della welcome page

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -13,7 +13,7 @@ const Welcome = () => {
             <div className="fixed top-0 left-0 h-screen w-full bg-black/80 flex z-20 flex-col items-center justify-center text-white">
                 <h1 className="text-6xl mb-4 font-semibold">QuickAlerts</h1>
                 <span className="font-light mb-4">Stay Informed, Stay Ahead</span>
-                <button className="border rounded-lg p-3 text-sm" onClick={() => {navigate('/home')}}>Unlock the power of information</button>
+                <button className="border rounded-lg p-3 text-sm dark:bg-gray-800 dark:text-gray-100 dark:border-none" onClick={() => { navigate('/home') }}>Unlock the power of information</button>
             </div>
 
 
