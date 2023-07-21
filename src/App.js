0100a64f640f10c3e80b34989a26673d006a19f4
andrea-buonocore <andreabuonocore00@gmail.com>
2023-07-21 16:14:21 +0200

    implementato redux-persist

--- src/App.js
+++ src/App.js
@@ -2,25 +2,27 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainContainer from "./components/MainContainer";
 import SavedArticles from "./components/SavedArticles";
 import { Provider } from "react-redux";
-import store from "./redux/store"
+import { store, persistor } from "./redux/store"
 import Settings from "./components/Settings";
 import Welcome from "./components/Welcome";
 import Header from "./components/Header";
-
+import { PersistGate } from 'redux-persist/integration/react';
 
 function App() {
   return (
     <Provider store={store}>
-      <div id="App">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Welcome />} />
-            <Route path="/saved" element={<SavedArticles />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/home" element={<MainContainer />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
+      <PersistGate loading={null} persistor={persistor}>
+        <div id="App">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Welcome />} />
+              <Route path="/saved" element={<SavedArticles />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/home" element={<MainContainer />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,19 +1,39 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // o qualsiasi altro storage desiderato, come AsyncStorage per React Native
 
-import SaveArticleReducer from "../reducers/SaveArticleReducer";
-import { configureStore } from "@reduxjs/toolkit";
-import CategoryReducer from "../reducers/CategoryReducer";
-import CountryReducer from "../reducers/CountryReducer";
+import SaveArticleReducer from '../reducers/SaveArticleReducer';
+import CategoryReducer from '../reducers/CategoryReducer';
+import CountryReducer from '../reducers/CountryReducer';
 
+// Configurazione di redux-persist
+const persistConfig = {
+    key: 'root', // La chiave in cui saranno memorizzati i dati nel tuo storage.
+    storage, // Il motore di storage da utilizzare, che abbiamo importato in precedenza.
+    // Se desideri configurare ulteriori opzioni, consulta la documentazione di redux-persist.
+};
 
+const rootReducer = combineReducers({
+    saveArticle: SaveArticleReducer,
+    category: CategoryReducer,
+    country: CountryReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middleware = getDefaultMiddleware({
+    // Puoi configurare qui eventuali middleware aggiuntivi che desideri utilizzare.
+    // Assicurati che redux-persist sia inserito prima di altri middleware per evitare problemi di persistenza.
+});
+
+// Aggiungi il middleware al createStore.
 const store = configureStore({
-    reducer: combineReducers({
+    reducer: persistedReducer,
+    middleware,
+});
 
-        saveArticle: SaveArticleReducer,
-        category: CategoryReducer,
-        country : CountryReducer
+// Crea il persistor una sola volta e lo esporta.
+const persistor = persistStore(store);
 
-    })
-})
-
-export default store;
+export { store, persistor };
